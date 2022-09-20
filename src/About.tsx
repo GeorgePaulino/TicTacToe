0f344d5e5d6be61786f31cc8b1d5e34978f8537a
George P <george7paulino@gmail.com>
2022-09-20 17:09:56 -0300

    responsive update

--- src/About.tsx
+++ src/About.tsx
@@ -8,7 +8,8 @@ function About() {
   return (
     <div className="About">
         <span>{lang['by']} <a href='https://www.instagram.com/george_psf/'>George P.</a></span> <br />
-        <span>{lang['design']} <a href='https://www.instagram.com/will.argate11/'>Will R.</a></span>
+        <span>{lang['design']} <a href='https://www.instagram.com/will.argate11/'>Will R.</a></span> <br />
+        <span>GitHub: <a href='https://github.com/GeorgePaulino/TicTacToe'>Tic Tac Toe</a></span>
         <div className='quotes'>
             <br />
             <div className='yada'>{lang['quotey']}</div> <br /> <br />
